1c29669bfdb964521a30cbbb1323165bfaceee61
bev1 <longer30001@gmail.com>
2020-10-19 13:34:28 +0300

    animate

--- src/js/script.js
+++ src/js/script.js
@@ -90,99 +90,119 @@ $( document ).ready(function() {
     }]
   });
   }
-  // setInterval(() => {
-
-
-
-    // console.log(document.querySelector('.rubiks-cube').getBoundingClientRect())
-
-
-  // }, 500);
-  $('.cube-wrapper').on({
-		mouseover: function(e) {
-      function getcsstransform(obj)
-      {
-        var isIE = /(MSIE|Trident\/|Edge\/)/i.test(navigator.userAgent);
+  
+  function getcsstransform(obj)
+  {
+    var isIE = /(MSIE|Trident\/|Edge\/)/i.test(navigator.userAgent);
+    
+    var TType="undefined",
+        rotateX = 0,
+        rotateY = 0,
+        rotateZ = 0;
         
-        var TType="undefined",
-            rotateX = 0,
-            rotateY = 0,
-            rotateZ = 0;
-            
-        var matrix = obj.css("-webkit-transform") ||
-          obj.css("-moz-transform") ||
-          obj.css("-ms-transform") ||
-          obj.css("-o-transform") ||
-          obj.css("transform");
-        if (matrix!==undefined && matrix !== 'none')
-        {
-          // if matrix is 2d matrix
-          TType="2D";
-          if (matrix.indexOf('matrix(') >= 0)
-          {
-            var values = matrix.split('(')[1].split(')')[0];
-            if (isIE)  //case IE
-            {
-              angle = parseFloat(values.replace('deg', STR_EMPTY));
-            }else
-            {
-              values = values.split(',');
-              var a = values[0];
-              var b = values[1];
-              var rotateZ = Math.round(Math.atan2(b, a) * (180 / Math.PI));
-            }
-          }else
-          {
-            // matrix is matrix3d
-            TType="3D";
-            var values = matrix.split('(')[1].split(')')[0].split(',');
-            var sinB = parseFloat(values[8]);
-            var b = Math.round(Math.asin(sinB) * 180 / Math.PI);
-            var cosB = Math.cos(b * Math.PI / 180);
-            var matrixVal10 = parseFloat(values[9]);
-            var a = Math.round(Math.asin(-matrixVal10 / cosB) * 180 / Math.PI);
-            var matrixVal1 = parseFloat(values[0]);
-            var c = Math.round(Math.acos(matrixVal1 / cosB) * 180 / Math.PI);
-            rotateX = a;
-            rotateY = b;
-            rotateZ = c;
-          }
-        }
-      
-        return  { TType: TType, rotateX: rotateX,  rotateY: rotateY,  rotateZ: rotateZ };
-      };
-      
-      mAngle = getcsstransform($(".rubiks-cube"));
-      if (mAngle.TType=="2D")
+    var matrix = obj.css("-webkit-transform") ||
+      obj.css("-moz-transform") ||
+      obj.css("-ms-transform") ||
+      obj.css("-o-transform") ||
+      obj.css("transform");
+    if (matrix!==undefined && matrix !== 'none')
+    {
+      // if matrix is 2d matrix
+      TType="2D";
+      if (matrix.indexOf('matrix(') >= 0)
       {
-        $("#Result").html("Transform 2D [rotateZ=" + mAngle.rotateZ + "&deg;]");
+        var values = matrix.split('(')[1].split(')')[0];
+        if (isIE)  //case IE
+        {
+          angle = parseFloat(values.replace('deg', STR_EMPTY));
+        }else
+        {
+          values = values.split(',');
+          var a = values[0];
+          var b = values[1];
+          var rotateZ = Math.round(Math.atan2(b, a) * (180 / Math.PI));
+        }
       }else
       {
-        $("#Result").html("Transform 3D [rotateX=" + mAngle.rotateX + "&deg;|rotateY=" + mAngle.rotateY + "&deg;|rotateZ=" + mAngle.rotateZ + "&deg;]");
+        // matrix is matrix3d
+        TType="3D";
+        var values = matrix.split('(')[1].split(')')[0].split(',');
+        var sinB = parseFloat(values[8]);
+        var b = Math.round(Math.asin(sinB) * 180 / Math.PI);
+        var cosB = Math.cos(b * Math.PI / 180);
+        var matrixVal10 = parseFloat(values[9]);
+        var a = Math.round(Math.asin(-matrixVal10 / cosB) * 180 / Math.PI);
+        var matrixVal1 = parseFloat(values[0]);
+        var c = Math.round(Math.acos(matrixVal1 / cosB) * 180 / Math.PI);
+        rotateX = a;
+        rotateY = b;
+        rotateZ = c;
       }
-      $('.rubiks-cube').addClass('default')
-      $('.rubiks-cube').css('-webkit-transform','rotateX('+mAngle.rotateX+'deg) rotateY('+mAngle.rotateY+'deg) rotateZ('+mAngle.rotateZ+'deg)')
-      $('.rubiks-cube').animate({  textIndent: 0 }, {
-        step: function(now,fx) {
-          $(this).css('-webkit-transform','rotateX('+26+'deg) rotateY('+-42+'deg) rotateZ('+3+'deg)'); 
-        },
-        duration: 'slow'
-    },'linear');
+    }
+  
+    return  { TType: TType, rotateX: rotateX,  rotateY: rotateY,  rotateZ: rotateZ };
+  };
+  
+  $('.cube-wrapper').hover(function() {
       
-      // .animate([
-      //   // keyframes
-      //   { transform: 'rotateX('+386+'deg) rotateY('+318+'deg) rotateZ('+357+'deg)' }, 
-      //   { transform: 'rotateX('+0+'deg) rotateY('+0+'deg) rotateZ('+0+'deg)' }
-      // ], {
-      //   // timing options
-      //   duration: 1000,
-      //   iterations: Infinity
-      // })
-      // $('.rubiks-cube').addClass('default')
-		},
-		mouseout: function() {
-      $('.rubiks-cube').removeClass('default')
-		}
-	});
+    mAngle = getcsstransform($(".rubiks-cube"));
+    $('.rubiks-cube').addClass('default')
+    $('.rubiks-cube').css('-webkit-transform','rotateX('+mAngle.rotateX+'deg) rotateY('+mAngle.rotateY+'deg) rotateZ('+mAngle.rotateZ+'deg)')
+    $('.rubiks-cube.default').animate({  textIndent: 0 }, {
+      step: function(now,fx) {
+        $(this).css({
+          '-webkit-transform':'rotateX('+26+'deg) rotateY('+-42+'deg) rotateZ('+3+'deg)',
+          '-moz-transform':'rotateX('+26+'deg) rotateY('+-42+'deg) rotateZ('+3+'deg)',
+          '-ms-transform':'rotateX('+26+'deg) rotateY('+-42+'deg) rotateZ('+3+'deg)',
+          '-o-transform':'rotateX('+26+'deg) rotateY('+-42+'deg) rotateZ('+3+'deg)',
+          'transform':'rotateX('+26+'deg) rotateY('+-42+'deg) rotateZ('+3+'deg)'}); 
+      },
+  });    
+  }, function() {
+    $('.rubiks-cube').removeAttr('style');
+    $('.rubiks-cube').removeClass('default')    
+  })
+
+  $('.rubiks-cube .detail:nth-child(1)').hover(function() {
+    $('a.cube-link.second').fadeIn()
+  },
+  function() {
+    $('a.cube-link.second').fadeOut()
+  })
+
+  $('.rubiks-cube .detail:nth-child(1)').on('click', function(e) {
+    $('html, body').animate({
+      scrollTop: $('#consulting-10').offset().top
+    }, 500);
+    return false;
+  });
+
+  $('.rubiks-cube .detail:nth-child(3)').hover(function() {
+    $('a.cube-link.first').fadeIn()
+  },
+  function() {
+    $('a.cube-link.first').fadeOut()
+  })
+
+  $('.rubiks-cube .detail:nth-child(3)').on('click', function(e) {
+    $('html, body').animate({
+      scrollTop: $('#consulting-1').offset().top
+    }, 500);
+    return false;
+  });
+
+  $('.rubiks-cube .detail:nth-child(20)').hover(function() {
+    $('a.cube-link.third').fadeIn()
+  },
+  function() {
+    $('a.cube-link.third').fadeOut()
+  })
+
+  $('.rubiks-cube .detail:nth-child(20)').on('click', function(e) {
+    $('html, body').animate({
+      scrollTop: $('#consulting-7').offset().top
+    }, 500);
+    return false;
+  });
 
 });
