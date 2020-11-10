03cafd0025639fd71b86ef65a38f50159d679314
bev1 <longer30001@gmail.com>
2020-11-10 14:02:53 +0200

    apdate

--- dist/js/cube.js
+++ dist/js/cube.js
@@ -9,14 +9,24 @@ renderer.setSize(1000, 500);
 renderer.setClearColor( 0xffffff, 0);
 wrapper.appendChild(renderer.domElement);
 const controls = new THREE.OrbitControls(camera, renderer.domElement);
-camera.position.set( 0, 0, 10 );
+camera.position.set( 6, -3, 8 );
 controls.enableZoom = false;
 controls.enablePan = false;
 controls.update();
-// Cubes
+const pointLight = new THREE.PointLight(0xffffff, 0.15, 400, 2);
+pointLight.position.set(3, 3, 3);
+const lightHolder = new THREE.Group();
+pointLight.castShadow = true;
+lightHolder.add(pointLight);
+scene.add(lightHolder);
+scene.add(new THREE.AmbientLight(0xffffff, 0.95));
 
+
+
+
+// Cubes
 const geometry = new THREE.CubeGeometry(1, 1, 1);
-const material = new THREE.MeshNormalMaterial({color: 0xdbdbdb});
+const material = new THREE.MeshPhongMaterial({color: 0xdbdbdb});
 
 const cubes = []; 
 
@@ -25,35 +35,90 @@ for(i=0; i<27; i++) {
 }
 
 const loader = new THREE.TextureLoader();
-
+// cube12
 loader.load('../img/cube1/money.png',
   function(texture) {
     const material12 = [ 
-      new THREE.MeshBasicMaterial({color:0xb71e45, transparent:true, opacity:1, side: THREE.DoubleSide}),
-      new THREE.MeshBasicMaterial({color:0xb71e45, transparent:true, opacity:1, side: THREE.DoubleSide}), 
-      new THREE.MeshBasicMaterial({color:0x7a9c16, transparent:true, opacity:1, side: THREE.DoubleSide}),
-      new THREE.MeshBasicMaterial({color:0x7a9c16, transparent:true, opacity:1, side: THREE.DoubleSide}), 
-      new THREE.MeshBasicMaterial({map: texture}), 
-      new THREE.MeshBasicMaterial({color:0x2e5398, transparent:true, opacity:0.8, side: THREE.DoubleSide}), 
+      new THREE.MeshPhongMaterial({color:0xb71e45, transparent:true, opacity:1, side: THREE.DoubleSide}),
+      new THREE.MeshPhongMaterial({color:0xb71e45, transparent:true, opacity:1, side: THREE.DoubleSide}), 
+      new THREE.MeshPhongMaterial({color:0x7a9c16, transparent:true, opacity:1, side: THREE.DoubleSide}),
+      new THREE.MeshPhongMaterial({color:0x7a9c16, transparent:true, opacity:1, side: THREE.DoubleSide}), 
+      new THREE.MeshPhongMaterial({map: texture}), 
+      new THREE.MeshPhongMaterial({color:0x2e5398, transparent:true, opacity:1, side: THREE.DoubleSide}), 
     ];
     const cube12 = new THREE.Mesh(geometry, material12);
     cube12.position.set(-1.1, 1.1, 1.1);
     scene.add(cube12);
   }
 );
+// cube14
+loader.load('../img/cube1/ribbon.png',
+  function(texture) {
+    const material14 = [ 
+      new THREE.MeshPhongMaterial({color:0xb71e45, transparent:true, opacity:1, side: THREE.DoubleSide}),
+      new THREE.MeshPhongMaterial({color:0xb71e45, transparent:true, opacity:1, side: THREE.DoubleSide}), 
+      new THREE.MeshPhongMaterial({color:0x7a9c16, transparent:true, opacity:1, side: THREE.DoubleSide}),
+      new THREE.MeshPhongMaterial({color:0x7a9c16, transparent:true, opacity:1, side: THREE.DoubleSide}), 
+      new THREE.MeshPhongMaterial({map: texture}), 
+      new THREE.MeshPhongMaterial({color:0x2e5398, transparent:true, opacity:1, side: THREE.DoubleSide}), 
+    ];
+    const cube14 = new THREE.Mesh(geometry, material14);
+    cube14.position.set(0, -1.1, 1.1);
+    scene.add(cube14);
+  }
+);
+// cube10
+loader.load('../img/cube1/accept.png',
+  function(texture) {
+    const material10 = [ 
+      new THREE.MeshPhongMaterial({color:0xb71e45, transparent:true, opacity:1, side: THREE.DoubleSide}),
+      new THREE.MeshPhongMaterial({color:0xb71e45, transparent:true, opacity:1, side: THREE.DoubleSide}), 
+      new THREE.MeshPhongMaterial({color:0x7a9c16, transparent:true, opacity:1, side: THREE.DoubleSide}),
+      new THREE.MeshPhongMaterial({color:0x7a9c16, transparent:true, opacity:1, side: THREE.DoubleSide}), 
+      new THREE.MeshPhongMaterial({map: texture}), 
+      new THREE.MeshPhongMaterial({color:0x2e5398, transparent:true, opacity:1, side: THREE.DoubleSide}), 
+    ];
+    const cube10 = new THREE.Mesh(geometry, material10);
+    cube10.position.set(1.1, 1.1, 1.1);
+    scene.add(cube10);
+  }
+);
+// cube15
+loader.load('../img/cube1/payment.png',
+  function(texture) {
+    const material15 = [ 
+      new THREE.MeshPhongMaterial({map: texture}),
+      new THREE.MeshPhongMaterial({color:0xdbdbdb, transparent:true, opacity:1, side: THREE.DoubleSide}), 
+      new THREE.MeshPhongMaterial({color:0xdbdbdb, transparent:true, opacity:1, side: THREE.DoubleSide}),
+      new THREE.MeshPhongMaterial({color:0xdbdbdb, transparent:true, opacity:1, side: THREE.DoubleSide}), 
+      new THREE.MeshPhongMaterial({color:0xdbdbdb, transparent:true, opacity:1, side: THREE.DoubleSide}), 
+      new THREE.MeshPhongMaterial({color:0xdbdbdb, transparent:true, opacity:1, side: THREE.DoubleSide}), 
+    ];
+    const cube15 = new THREE.Mesh(geometry, material15);
+    cube15.position.set(1.1, 0, 1.1);
+    scene.add(cube15);
+  }
+);
+// cube24
+loader.load('../img/cube1/location.png',
+  function(texture) {
+    const material24 = [ 
+      new THREE.MeshPhongMaterial({map: texture}),
+      new THREE.MeshPhongMaterial({color:0xdbdbdb, transparent:true, opacity:1, side: THREE.DoubleSide}), 
+      new THREE.MeshPhongMaterial({color:0xdbdbdb, transparent:true, opacity:1, side: THREE.DoubleSide}),
+      new THREE.MeshPhongMaterial({color:0xdbdbdb, transparent:true, opacity:1, side: THREE.DoubleSide}), 
+      new THREE.MeshPhongMaterial({color:0xdbdbdb, transparent:true, opacity:1, side: THREE.DoubleSide}), 
+      new THREE.MeshPhongMaterial({color:0xdbdbdb, transparent:true, opacity:1, side: THREE.DoubleSide}), 
+    ];
+    const cube24 = new THREE.Mesh(geometry, material24);
+    cube24.position.set(1.1, 0, -1.1);
+    scene.add(cube24);
+  }
+);
 
 
 cubes.forEach((cube, i)=> {
-  if(i===12) {
-    // loader.load( '../img/cube1/money.png',
-    //   function(texture) {
-    //     const material12 = new THREE.MeshBasicMaterial( { map: texture } );
-    //     const cube12 = new THREE.Mesh(geometry, material12);
-    //     cube12.position.set(-1.1, 1.1, 1.1);
-    //     scene.add(cube12);
-    //   }
-    // );
-  } else {
+  if(i != 12 || i != 10 || i != 14) {
     scene.add(cube);
   }
   switch(i) {
@@ -84,9 +149,9 @@ cubes.forEach((cube, i)=> {
     case 9:
       cube.position.set(0, 0, 1.1);
       break;
-    case 10:
-      cube.position.set(1.1, 1.1, 1.1);
-      break;
+    // case 10:
+    //   cube.position.set(1.1, 1.1, 1.1);
+    //   break;
     case 11:
       cube.position.set(-1.1, -1.1, 1.1);
       break;
@@ -96,12 +161,12 @@ cubes.forEach((cube, i)=> {
     case 13:
       cube.position.set(1.1, -1.1, 1.1);
       break;
-    case 14:
-      cube.position.set(0, -1.1, 1.1);
-      break;
-    case 15:
-      cube.position.set(1.1, 0, 1.1);
-      break;
+    // case 14:
+    //   cube.position.set(0, -1.1, 1.1);
+    //   break;
+    // case 15:
+    //   cube.position.set(1.1, 0, 1.1);
+    //   break;
     case 16:
       cube.position.set(-1.1, 0, 1.1);
       break;
@@ -126,9 +191,9 @@ cubes.forEach((cube, i)=> {
     case 23:
       cube.position.set(0, -1.1, -1.1);
       break;
-    case 24:
-      cube.position.set(1.1, 0, -1.1);
-      break;
+    // case 24:
+    //   cube.position.set(1.1, 0, -1.1);
+    //   break;
     case 25:
       cube.position.set(-1.1, 0, -1.1);
       break;
@@ -138,15 +203,14 @@ cubes.forEach((cube, i)=> {
   }
 });
 
-// const light = new THREE.DirectionalLight(0xffffff, 1.0, 500);
-// light.position.set( 1, 1, 1 );
-// scene.add(light)
-
 
 function animate() {
-	requestAnimationFrame( animate );
+  requestAnimationFrame( animate );
+  lightHolder.quaternion.copy(camera.quaternion);
 	controls.update();
 	renderer.render( scene, camera );
+  console.log("animate -> scene", scene);
+  
 }
 
 animate();
